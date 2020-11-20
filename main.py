a5cc8ab59eeadf18cf5b0bcaec5d4a881967e7f0
TheLony97 <jakob.lonauer@outlook.de>
2020-11-20 12:20:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,22 @@ def generate_password(nchar: int) -> str:
 	password: str = "" 
 	for e in range(nchar):
 		password = password +  str(random.randint(1,nchar))
+
+	if text == True:
+		chars: list = [] 
+		for e in range(5):
+			chars.append(random.choice(string.ascii_lowercase))
+		password = password + "".join(chars)
+
+	if shuffle == True:
+		new_password: str = ""
+		for x in range(len(password)):
+			new_password += random.choice(password)
+		password = new_password
+
 	return password
 
 password = generate_password(8)
+print(password)password = generate_password(8,True,True)
 print(password)
+
