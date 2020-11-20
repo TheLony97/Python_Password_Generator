c2efa8d1001e5640be398b4f9d4867c655a835ba
TheLony97 <jakob.lonauer@outlook.de>
2020-11-20 11:56:41 +0100

    Update main.py
    Completed The Password Generator

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import random
 
 def generate_password(nchar: int) -> str:
 
-
-	password
+	password: str = "" 
+	for e in range(nchar):
+		password = password +  str(random.randint(1,nchar))
 	return password
+
+password = generate_password(8)
+print(password)
